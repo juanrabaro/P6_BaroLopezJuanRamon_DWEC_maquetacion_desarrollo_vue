f327f2994df192b6c8d45077d6ac6070d0cbdaf1
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-02-23 12:20:31 +0100

    añadiendo datos de prueba como si fuera la api

---
+++ src/assets/exampleData/tiempoCiudades.js
@@ -0,0 +1,247 @@
+export const exampleDataCiudades =
+{
+  "data": [
+    {
+      "id": 1,
+      "nombreCiudad": "Cádiz",
+      "poblacion": 115000,
+      "terreno": 13,
+      "urlImagen": "https://cadizturismo.com/storage/app/media/uploaded-files/p-cadiz_turismo.jpg",
+      "tiempoCiudad": {
+        "id": 1,
+        "idCiudad": 1,
+        "temperaturaMax": 17,
+        "temperaturaMin": 12,
+        "humedad": 88,
+        "viento": 10,
+        "precipitacion": 0
+      },
+      "zonaHoraria": {
+        "id": 1,
+        "idCiudad": 1,
+        "horaZona": 0
+      }
+    },
+    {
+      "id": 2,
+      "nombreCiudad": "Pamplona",
+      "poblacion": 200000,
+      "terreno": 25,
+      "urlImagen": "https://navarra.okdiario.com/media/navarra/images/2022/07/13/2022071319512376624.jpg",
+      "tiempoCiudad": {
+        "id": 2,
+        "idCiudad": 2,
+        "temperaturaMax": 16,
+        "temperaturaMin": -1,
+        "humedad": 71,
+        "viento": 10,
+        "precipitacion": 0
+      },
+      "zonaHoraria": {
+        "id": 2,
+        "idCiudad": 2,
+        "horaZona": 0
+      }
+    },
+    {
+      "id": 3,
+      "nombreCiudad": "Jerez",
+      "poblacion": 210000,
+      "terreno": 1188,
+      "urlImagen": "https://images.hola.com/imagenes/viajes/20201026177968/48-horas-en-jerez-de-la-frontera/0-882-189/jerez-de-la-frontera-plaza-asuncion-t.jpg?tx=w_680",
+      "tiempoCiudad": {
+        "id": 3,
+        "idCiudad": 3,
+        "temperaturaMax": 18,
+        "temperaturaMin": 9,
+        "humedad": 95,
+        "viento": 8,
+        "precipitacion": 0
+      },
+      "zonaHoraria": {
+        "id": 3,
+        "idCiudad": 3,
+        "horaZona": 0
+      }
+    },
+    {
+      "id": 4,
+      "nombreCiudad": "Madrid",
+      "poblacion": 3300000,
+      "terreno": 600,
+      "urlImagen": "https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2023/01/24/istock-1297090032.jpeg",
+      "tiempoCiudad": {
+        "id": 4,
+        "idCiudad": 4,
+        "temperaturaMax": 13,
+        "temperaturaMin": 3,
+        "humedad": 93,
+        "viento": 18,
+        "precipitacion": 0
+      },
+      "zonaHoraria": {
+        "id": 4,
+        "idCiudad": 4,
+        "horaZona": 0
+      }
+    },
+    {
+      "id": 5,
+      "nombreCiudad": "Londres",
+      "poblacion": 9000000,
+      "terreno": 1500,
+      "urlImagen": "https://www.iese.edu/es/insight/wp-content/uploads/sites/4/2022/11/Londres-Cities-in-motion-1024-alto.jpg",
+      "tiempoCiudad": {
+        "id": 5,
+        "idCiudad": 5,
+        "temperaturaMax": 12,
+        "temperaturaMin": 4,
+        "humedad": 87,
+        "viento": 27,
+        "precipitacion": 45
+      },
+      "zonaHoraria": {
+        "id": 5,
+        "idCiudad": 5,
+        "horaZona": -1
+      }
+    },
+    {
+      "id": 6,
+      "nombreCiudad": "Buenos Aires",
+      "poblacion": 16000000,
+      "terreno": 200,
+      "urlImagen": "https://media.traveler.es/photos/62540d7e027033bdc5da0d1e/4:3/w_2662,h_1997,c_limit/_buenos-aires-gettyimages-743738515.jpg",
+      "tiempoCiudad": {
+        "id": 6,
+        "idCiudad": 6,
+        "temperaturaMax": 29,
+        "temperaturaMin": 22,
+        "humedad": 76,
+        "viento": 16,
+        "precipitacion": 0
+      },
+      "zonaHoraria": {
+        "id": 6,
+        "idCiudad": 6,
+        "horaZona": -4
+      }
+    },
+    {
+      "id": 7,
+      "nombreCiudad": "Frankfurt",
+      "poblacion": 750000,
+      "terreno": 248,
+      "urlImagen": "https://www.germany.travel/media/redaktion/staedte_kultur_content/frankfurt/fuer_gallerydomblick_frankfurtdavid_vasicek.jpg",
+      "tiempoCiudad": {
+        "id": 7,
+        "idCiudad": 7,
+        "temperaturaMax": 12,
+        "temperaturaMin": 4,
+        "humedad": 94,
+        "viento": 23,
+        "precipitacion": 10
+      },
+      "zonaHoraria": {
+        "id": 7,
+        "idCiudad": 7,
+        "horaZona": 0
+      }
+    },
+    {
+      "id": 8,
+      "nombreCiudad": "Florencia",
+      "poblacion": 400000,
+      "terreno": 100,
+      "urlImagen": "https://tourismmedia.italia.it/is/image/mitur/20210401173629-firenze-toscana-gettyimages-1145040590-1?wid=1080&hei=660&fit=constrain,1&fmt=webp",
+      "tiempoCiudad": {
+        "id": 8,
+        "idCiudad": 8,
+        "temperaturaMax": 15,
+        "temperaturaMin": 9,
+        "humedad": 76,
+        "viento": 8,
+        "precipitacion": 70
+      },
+      "zonaHoraria": {
+        "id": 8,
+        "idCiudad": 8,
+        "horaZona": 0
+      }
+    },
+    {
+      "id": 9,
+      "nombreCiudad": "Sevilla",
+      "poblacion": 700000,
+      "terreno": 141,
+      "urlImagen": "https://multimedia.andalucia.org/media/0BC700DB844F4EDFBE00C1FA9B493D71/img/1112772E6D5945A1B89C26E539DD0D99/SE_Catedral_01.jpg?responsive",
+      "tiempoCiudad": {
+        "id": 9,
+        "idCiudad": 9,
+        "temperaturaMax": 19,
+        "temperaturaMin": 9,
+        "humedad": 91,
+        "viento": 6,
+        "precipitacion": 0
+      },
+      "zonaHoraria": {
+        "id": 9,
+        "idCiudad": 9,
+        "horaZona": 0
+      }
+    },
+    {
+      "id": 10,
+      "nombreCiudad": "Málaga",
+      "poblacion": 600000,
+      "terreno": 400,
+      "urlImagen": "https://upload.wikimedia.org/wikipedia/commons/4/40/M%C3%A1laga_-_vistas_desde_el_Parador_de_Gibralfaro_01_%28cropped%29.jpg",
+      "tiempoCiudad": {
+        "id": 10,
+        "idCiudad": 10,
+        "temperaturaMax": 23,
+        "temperaturaMin": 11,
+        "humedad": 51,
+        "viento": 14,
+        "precipitacion": 0
+      },
+      "zonaHoraria": {
+        "id": 10,
+        "idCiudad": 10,
+        "horaZona": 0
+      }
+    }
+  ],
+  "links": {
+    "first": "http://localhost/api/ciudades?page=1",
+    "last": "http://localhost/api/ciudades?page=1",
+    "prev": null,
+    "next": null
+  },
+  "meta": {
+    "current_page": 1,
+    "from": 1,
+    "last_page": 1,
+    "links": [
+      {
+        "url": null,
+        "label": "&laquo; Previous",
+        "active": false
+      },
+      {
+        "url": "http://localhost/api/ciudades?page=1",
+        "label": "1",
+        "active": true
+      },
+      {
+        "url": null,
+        "label": "Next &raquo;",
+        "active": false
+      }
+    ],
+    "path": "http://localhost/api/ciudades",
+    "per_page": 10,
+    "to": 10,
+    "total": 10
+  }
+}
--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -3,7 +3,8 @@
 // import LogInForm from '../childComponents/LogInForm.vue';
 import UserControlForm from '../childComponents/UserControlForm.vue';
 
-import { exampleData } from '../../assets/exampleData/tiempoCiudad.js';
+import { exampleDataCiudades } from '../../assets/exampleData/tiempoCiudades.js';
+import { exampleDataUsuarios } from '../../assets/exampleData/usuarios.js';
 
 export default {
   name: 'HomePage',
@@ -15,6 +16,7 @@ export default {
   data() {
     return {
       exampleCityTimeData: null,
+      exampleUsersData: null,
       temperatureConclusionResult: String,
     }
   },
@@ -31,9 +33,9 @@ export default {
     },
     // determina según la temperatura el estado del tiempo
     temperatureConclusion() {
-      if (this.exampleCityTimeData.temperatura > 20) {
+      if (this.exampleCityTimeData[0].tiempoCiudad.temperaturaMin > 20) {
         this.temperatureConclusionResult = "Sunny";
-      } else if (this.exampleCityTimeData.temperatura > 15) {
+      } else if (this.exampleCityTimeData[0].tiempoCiudad.temperaturaMin > 15) {
         this.temperatureConclusionResult = "Cloudy";
       } else {
         this.temperatureConclusionResult = "Cold";
@@ -42,8 +44,10 @@ export default {
     // se ejecuta cuando se le da a buscar
     // en el futuro tendrá las funcionalidades correspondientes
     search() {
-      this.exampleCityTimeData = exampleData[0];
+      this.exampleCityTimeData = exampleDataCiudades.data;
+      this.exampleUsersData = exampleDataUsuarios.data;
       this.temperatureConclusion();
+      console.log(this.exampleCityTimeData[0]);
     },
   },
   created() {
@@ -60,7 +64,7 @@ export default {
       <p>Get detailed weather and time information</p>
       <input type="text" placeholder="Search for a city">
       <button v-on:click="search">Search</button>
-      <p v-if="exampleCityTimeData" class="result">Resultados para <span>Cádiz</span></p>
+      <p v-if="exampleCityTimeData" class="result">Resultados para <span>{{ exampleCityTimeData[0].nombreCiudad }}</span></p>
     </section>
     
     <section class="weather-details">
@@ -73,19 +77,33 @@ export default {
         <div class="temperature">
           <h3>Temperature</h3>
           <p>Current temperature in ºC</p>
-          <p v-if="exampleCityTimeData"><strong>{{ exampleCityTimeData.temperatura }}ºC</strong></p>
+          <p v-if="exampleCityTimeData"><strong>{{ exampleCityTimeData[0].tiempoCiudad.temperaturaMax }}ºC - {{ exampleCityTimeData[0].tiempoCiudad.temperaturaMin }}ºC</strong></p>
           <p v-if="exampleCityTimeData" class="conclusion">{{ temperatureConclusionResult }}</p>
         </div>
         <div class="humidity">
           <h3>Humidity</h3>
           <p>Current humidity level</p>
-          <p v-if="exampleCityTimeData"><strong>{{ exampleCityTimeData.humedad }}%</strong></p>
+          <p v-if="exampleCityTimeData"><strong>{{ exampleCityTimeData[0].tiempoCiudad.humedad }}%</strong></p>
         </div>
         <div class="wind">
           <h3>Wind Speed</h3>
           <p>Current wind speed in km/h</p>
-          <p v-if="exampleCityTimeData"><strong>{{ exampleCityTimeData.viento }}km/h</strong></p>
+          <p v-if="exampleCityTimeData"><strong>{{ exampleCityTimeData[0].tiempoCiudad.viento }}km/h</strong></p>
         </div>
+        <div class="rain">
+          <h3>Rain</h3>
+          <p>Probabilities of rain</p>
+          <p v-if="exampleCityTimeData"><strong>{{ exampleCityTimeData[0].tiempoCiudad.precipitacion }}%</strong></p>
+        </div>
+      </div>
+    </section>
+    <section v-if="exampleCityTimeData" class="city-img-section">
+      <div class="city-img-left-side">
+        <h2 v-if="exampleCityTimeData"><span>{{ exampleCityTimeData[0].nombreCiudad }}</span></h2>
+        <p v-if="exampleCityTimeData">This is an image of the beautiful city {{ exampleCityTimeData[0].nombreCiudad }}</p>
+      </div>
+      <div class="city-img-right-side">
+        <img href="exampleCityTimeData[0].urlImagen" alt="city image">
       </div>
     </section>
     <section v-if="!isLogged" class="log-section">
@@ -245,6 +263,62 @@ export default {
 
           }
         }
+        .rain {
+          border: 2px solid rgb(142, 142, 187);
+          border-radius: 8px;
+          padding: 15px 15px 15px 15px;
+          margin-bottom: 15px;
+
+          h3 {
+            font-size: 22px;
+            margin-top: 0;
+            margin-bottom: 12px;
+          }
+          p {
+            margin-top: 5px;
+            margin-bottom: 5px;
+
+          }
+        }
+      }
+    }
+
+    .city-img-section {
+      @include flex(row, center, center, 60px);
+      border-top: 3px solid rgb(36, 49, 98);
+      padding-top: 30px;
+      padding-bottom: 20px;
+
+      .city-img-left-side {
+        width: 30%;
+        padding: 15px 15px 15px 15px;
+        
+        h2 {
+          font-size: 35px;
+          margin-top: 0;
+          margin-bottom: 0;
+
+          span {
+            font-family: 'Lato Black';
+            color: rgb(8, 97, 181);
+          }
+        }
+        
+        p {
+          margin-top: 5px;
+          font-size: 20px;
+        }
+      }
+      .city-img-right-side {
+        text-align: center;
+        width: 30%;
+        padding: 15px 15px 15px 15px;
+        
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 8px;
+        }
       }
     }
 
@@ -257,4 +331,4 @@ export default {
       margin-bottom: 150px;
     }
   }
-</style>
+</style>../../assets/exampleData/tiempoCiudades.js
